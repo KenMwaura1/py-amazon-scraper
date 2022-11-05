7bbdbffe93f2b01e6bbba3fc59c63da102927865
Ken_Mwaura1 <Ken_Mwaura1>
2022-11-05 22:45:12 +0300

    updated scrapy pipelines to use Postgres

--- amz_scraper/amz_scraper/settings.py
+++ amz_scraper/amz_scraper/settings.py
@@ -62,9 +62,9 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'amz_scraper.pipelines.AmzScraperPipeline': 300,
-# }
+ITEM_PIPELINES = {
+    'amz_scraper.pipelines.AmzScraperPipeline': 300,
+ }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
