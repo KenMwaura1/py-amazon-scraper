ac38ae53eebc038ba94ce39cd6fe7294ae111192
Ken_Mwaura1 <Ken_Mwaura1>
2022-11-05 19:15:11 +0300

    Updated the parse item function for each item

--- amz_scraper/amz_scraper/spiders/amznspider.py
+++ amz_scraper/amz_scraper/spiders/amznspider.py
@@ -1,17 +1,25 @@
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from ..items import AmzScraperItem
+from scrapy.loader import ItemLoader
 
 
 class AmznspiderSpider(CrawlSpider):
     name = 'amznspider'
     allowed_domains = ['amazon.com']
-    start_urls = ['http://amazon.com/s?k=laptops']
+    start_urls = ['https://amazon.com/s?k=laptop']
 
     rules = (
-        Rule(LinkExtractor(allow='s?k=laptops&page=', restrict_css="a.s-pagination-next")),
+        Rule(LinkExtractor(allow='s?k=laptop&page=', restrict_css="a.s-pagination-next")),
         Rule(LinkExtractor(allow='/dp/'), callback='parse-item'),
     )
 
     def parse_item(self, response):
-        pass
+        l = ItemLoader(item=AmzScraperItem(), response=response)
+        l.add_css("name", "span#ProductTitle")
+        l.add_css("asin", "#ASIN::attr(value)")
+        l.add_css("price","span.a-offscreen")
+        l.add_css("discounted", "span.savingsPercentage")
+        l.add_css("totalreviews", "span#acrCustomerReviewText")
+        yield l.load_item()
