5f561097e8e43190d9fd09af17809bf348dcecc5
Ken_Mwaura1 <Ken_Mwaura1>
2022-11-05 19:19:55 +0300

    Updated the parse item function for each item

--- amz_scraper/amz_scraper/spiders/amznspider.py
+++ amz_scraper/amz_scraper/spiders/amznspider.py
@@ -12,14 +12,14 @@ class AmznspiderSpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(allow='s?k=laptop&page=', restrict_css="a.s-pagination-next")),
-        Rule(LinkExtractor(allow='/dp/'), callback='parse-item'),
+        Rule(LinkExtractor(allow='/dp/'), callback='parse_item'),
     )
 
     def parse_item(self, response):
         l = ItemLoader(item=AmzScraperItem(), response=response)
         l.add_css("name", "span#ProductTitle")
         l.add_css("asin", "#ASIN::attr(value)")
-        l.add_css("price","span.a-offscreen")
+        l.add_css("price", "span.a-offscreen")
         l.add_css("discounted", "span.savingsPercentage")
         l.add_css("totalreviews", "span#acrCustomerReviewText")
         yield l.load_item()
