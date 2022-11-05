858df81af46d055014c3f10f0ad16455eb2e9685
Ken_Mwaura1 <Ken_Mwaura1>
2022-11-05 18:08:24 +0300

    Added the code for processing the individual scraped items

--- amz_scraper/amz_scraper/items.py
+++ amz_scraper/amz_scraper/items.py
@@ -4,9 +4,16 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
 
 
 class AmzScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+   name = scrapy.Field(
+       input_processor=MapCompose(remove_tags),
+       output_processor=TakeFirst()
+   )
+   asin = scrapy.Field()
+   price = scrapy.Field()
+   discounted = scrapy.Field()
+   total_reviews = scrapy.Field()
